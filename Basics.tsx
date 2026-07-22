c38254046759ba55167c7a8d2c0c08b35d009cea
gullsnobar <abdulrehmanali7775@gmail.com>
2026-07-22 16:40:38 +0500

    did some examples

--- Basics.tsx
+++ Basics.tsx
@@ -15,5 +15,12 @@ let age: number = 23;
 let isStudent: boolean = true;
 let hobbies: string[] = ["reading", "coding"];
 let score: number | null = null;
+let city: string = "Karachi"
 
+// Topic 2: Data Types in TypeScript
 
+let id: number = 567;
+let nameName: string = "Snobar";
+let isActive: boolean = false;
+let notAssigned: undefined = undefined;
+let empty: null = null;
