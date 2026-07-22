046a29284eb36cf8242c2629c345dd05223f25c8
gullsnobar <abdulrehmanali7775@gmail.com>
2026-07-22 16:32:36 +0500

    did practice with variables

--- Basics.tsx
+++ Basics.tsx
@@ -6,3 +6,14 @@ function greet(name: string): string {
 
 const message: string = greet("World");
 console.log(message);
+
+
+// Varibales in TypeScript
+
+let username: string = "Ali";
+let age: number = 23;
+let isStudent: boolean = true;
+let hobbies: string[] = ["reading", "coding"];
+let score: number | null = null;
+
+
