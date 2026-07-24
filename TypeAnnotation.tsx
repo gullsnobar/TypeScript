f608c81ca8f28eb0b5487f2ebeab699baf821063
gullsnobar <abdulrehmanali7775@gmail.com>
2026-07-24 23:01:19 +0500

    type annotation done

---
+++ TypeAnnotation.tsx
@@ -0,0 +1,10 @@
+// What is Type Annotation
+
+// Type Annotation ka matlab hai TypeScript ko pehle hi batana ke is variable mein kis type ki value hi store hogi.
+// colon kay bad hum type define karty hain.
+// TS static type ha.
+
+let firstname: string = "Snobar";
+let isLoggedIn: boolean = true;
+let myage: number = 67;
+
