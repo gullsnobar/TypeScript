657121299a3c81820595443ad723d690abb88d41
gullsnobar <abdulrehmanali7775@gmail.com>
2026-07-25 22:34:47 +0500

    type inference still working

--- TypeAnnotation.tsx
+++ TypeAnnotation.tsx
@@ -1,10 +1,37 @@
 // What is Type Annotation
 
 // Type Annotation ka matlab hai TypeScript ko pehle hi batana ke is variable mein kis type ki value hi store hogi.
+
 // colon kay bad hum type define karty hain.
+
 // TS static type ha.
 
-let firstname: string = "Snobar";
+let studentname: string = "Snobar";
 let isLoggedIn: boolean = true;
 let myage: number = 67;
+
+
+console.log(studentname);
+
+studentname = "Muhammad Tayyyab";
+myage = 23;
+
+
+
+// TypeScript
+
+// Run se pehle error dikha do
+
+// Isi wajah se TypeScript ko safer language kehte hain.
+
+
+// TypeScript: Type Inference
+
+
+// Type Inference ka matlab hai TypeScript khud automatically determine kar leti hai ke kisi variable ki type kya hai, bina humein explicitly type likhne ki zaroorat ke.
+
+
+let nameStudent = "Dadood"
+
+
 
